ac364f4532e6a831c4cc13f66b90ffdb72a54cd1
Joel Cerbito <joel.cerbito03@gmail.com>
2025-11-19 16:29:48 +0800

    playwright config update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig, devices } from '@playwright/test';
+import configData from './config.json';
 
 export default defineConfig({
   testDir: './tests',
   retries: 1,
   use: {
     headless: process.env.CI ? true : false, // true in GitHub Actions, false locally
+    baseURL: configData.baseURL,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     screenshot: 'only-on-failure',
